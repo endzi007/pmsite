a9f6a9285b42d05df86f7148e6c7f60c2cfc325e
endzi00 <jasarovic.enis@gmail.com>
2018-05-06 21:46:40 +0200

    added hover image

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -4,56 +4,31 @@ import LargeProject from './largeProject';
 import { style, cssRaw } from 'typestyle';
 
 
-var projectItem = style({
+
+const Project = (props)=> {
+    var projectItem = style({
         border: "1px solid #ffc34c",
-        height: "auto",
+        height: "250px",
         overflow: "hidden",
         position: "relative",
-});
-
-var projectHover = style({
-        color: "white",
-        position: "absolute",
-        padding: "4px",
-        left: 0,
-        top: 0,
-        width: "100%",
-        height: "100%",
-        backgroundColor: "#3c444b",
-        transition: "all 500ms",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around"
-});
-
-
-class Project extends Component {
-    constructor(){
-        super();
-        this.state = {
-            opacity: 0,
-            showModal: false
+        backgroundImage: `url(${props.featured_image})`,
+        backgroundSize: "cover",
+        transition: "all .5s linear",
+        $nest: {
+            "&:hover": {
+                backgroundImage: `url(${props.hover})`     
+            }
         }
-    }
-    toggleModalShow (arg){
-        let opacity;
-        if(arg === false){
-            opacity = 0;
-        }
-        this.setState({ opacity: opacity, showModal: arg});
-    }
-    render(){
-        return(
-            <div className={projectItem} 
-                onMouseOver={(e)=>{this.setState({opacity: 1});}}
-                onMouseLeave={()=>{this.setState({opacity: 0});}}
-            >
-                <img src={this.props.featured_image} alt={this.props.title}/>
-                <LargeProject show={this.state.showModal} toggleModalShow ={this.toggleModalShow.bind(this, false)} {...this.props} />
-            </div>
-        );
-    }
-}
+    });
+
+    return(
+        <div className={projectItem}>
+            {props.title}
+            <LargeProject show={false} {...props} />
+        </div>
+    );
+ }
+
 
 
 export default Project;
